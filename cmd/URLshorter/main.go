efd3c2781f6e779d918760c5d688a45a1800ee1f
Dister <Levchikov.zhenya@mail.ru>
2024-09-05 16:19:24 +0700

    create DB and middleware

--- cmd/URLshorter/main.go
+++ cmd/URLshorter/main.go
@@ -2,22 +2,65 @@ package main
 
 import (
 	"URLshorter/internal/config"
-	"fmt"
+	"URLshorter/internal/lib/logger/sl"
+	"URLshorter/internal/storage/sqlite"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
 
-	// TODO: init config: cleanenv
+	log.Info("starting url shorter", slog.String("key", cfg.Env))
+	log.Debug("debug messages are enable")
 
-	// TODO: init longer: slog
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to create sqlite storage", sl.Err(err))
+		os.Exit(1)
+	}
 
-	// TODO: init storage: sqlite
+	_ = storage
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
 
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})
+	}
+
+	return log
 }
